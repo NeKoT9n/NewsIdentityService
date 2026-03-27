28e539b1179a28227816be1fd62f4ac9c1d4dd46
Ne Artem <artemblajievskiy@gmail.com>
2026-03-27 14:00:33 +0300

    Add models

---
+++ IdentityService.Domain/Models/RefreshToken.cs
@@ -0,0 +1,18 @@
+﻿namespace IdentityService.Domain.Models;
+
+public class RefreshToken 
+{
+    public Guid Id { get; set; }
+    
+    public string Token { get; set; } = string.Empty;
+
+    public DateTime ExpiresAt { get; set; }
+
+    public bool IsRevoked { get; set; }
+
+    public bool IsExpired => DateTime.UtcNow >= ExpiresAt;
+
+    public bool IsActive => !IsRevoked && !IsExpired;
+
+    public virtual User User { get; set; } = null!;
+}
---
+++ IdentityService.Domain/Models/Role.cs
@@ -0,0 +1,10 @@
+﻿namespace IdentityService.Domain.Models;
+
+public class Role
+{
+    public long Id { get; set; } 
+    
+    public string Name { get; set; } = string.Empty;
+    
+    public string? Description { get; set; }
+}
