ad7c1ca37b9af17f8e7b02e4d32969b2a0fcfe39
Ne Artem <artemblajievskiy@gmail.com>
2026-03-27 14:18:59 +0300

    Add dbContext

---
+++ IdentityService.DataAccess/IdentityDbContext.cs
@@ -0,0 +1,18 @@
+﻿using IdentityService.DataAccess.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace IdentityService.DataAccess;
+
+public class IdentityDbContext(DbContextOptions<IdentityDbContext> options) : DbContext(options)
+{
+    public DbSet<UserEntity> Users => Set<UserEntity>();
+    public DbSet<RoleEntity> Roles => Set<RoleEntity>();
+    public DbSet<RefreshTokenEntity> RefreshTokens => Set<RefreshTokenEntity>();
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+        
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(IdentityDbContext).Assembly);
+    }
+}
